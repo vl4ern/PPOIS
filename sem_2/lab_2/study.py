563d3baecc154fceaa4831723523002e5b68d04a
Xelit <bitt9ra@gmail.com>
2026-02-27 13:07:23 +0300

    11

--- sem_2/lab_2/study.py
+++ sem_2/lab_2/study.py
@@ -1,26 +1,79 @@
-def get_rubles():
-    while True:
-        try:
-            rubles = int(input("Введите количество рублей: "))
-            if rubles > 0:
-                return rubles
-            else:
-                print("Надо ввести положительное число!")
-        except ValueError:
-            print("Надо ввести целое число.")
+class Hero:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.damage = 5
+        self._health = 100
+        self.level = 1
 
-def convert_to_usd(rubles, rate):
-    balance_usd = rubles * rate
-    return balance_usd
+    def __str__(self):
+        return f"Class:{self.__class__.__name__} | Name: {self.name} | HP: {self.health}"
 
-def show_balance(usd_amount):
-    print(f"Ваш баланс в долларах: ${usd_amount:.2f}.")
+    @property
+    def health(self) -> int:
+        return self._health
+    
+    @health.setter
+    def health(self, new_value:int) -> int:
+        if new_value < 0:
+            self._health = 0
+            print(f"{self.name} death!")
+        elif new_value > 100:
+            self._health = 100
+        else:
+            self._health = new_value
+    
+    def attack(self, enemy: 'Hero') -> None:
+        print(f"{self.name} attacks {enemy.name} and deals {self.damage} damage.")
+        enemy.health -= self.damage
+
+class Warrior(Hero):
+    def __init__(self, name):
+        super().__init__(name)
+        self.damage = 15
+
+    def shout(self):
+        print(f"{self.name}: for the horde!!!")
+
+class Mage(Hero):
+    def __init__(self, name):
+        super().__init__(name)
+        self.damage = 3
+        self._mana = 100
+
+    def __str__(self):
+        return f"Class:{self.__class__.__name__} | Name: {self.name} | HP: {self.health} | Mana: {self.mana}"
+
+    @property
+    def mana(self):
+        return self._mana
+    
+    @mana.setter
+    def mana(self, new_value):
+        if new_value < 0:
+            self._mana = 0
+            print("Death!")
+        elif new_value > 100:
+            self._mana = 100
+        else:
+            self._mana = new_value
+
+    def cast_spell(self, enemy):
+        if self.mana >= 20:
+            print(f"{self.name} cast 'The thunderbolt' for 30 damage.")
+            enemy.health -= 30
+            self.mana -= 20
+        else:
+            print(f"Don't have enoughtf mana.")
 
 def main():
-    current_rate = 3.3
-    my_money = get_rubles()
-    balance_usd = convert_to_usd(my_money, current_rate)
-    show_balance(balance_usd)
+    conan = Warrior("Conan")
+    gendalf = Mage("Gendalf")
+    conan.attack(gendalf)
+    gendalf.cast_spell(conan)
+    conan.shout()
+    print(f"Gendalf: {gendalf.health}")
+    print(f"Conan: {conan.health}")  
+    print(gendalf.mana)
 
 if __name__ == "__main__":
     main()
